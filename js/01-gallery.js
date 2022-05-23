5f5b680adbe8529fb7e6d077111f3f8703cc6b16
Andrgoit <stakgrom@gmail.com>
2022-05-23 12:42:00 +0300

    add gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector("div.gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+galleryContainer.addEventListener("click", openGalleryItem);
+
+function openGalleryItem(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" alt="${e.target.dataset.description}">
+  `);
+
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector("ul.gallery");
+const galleryMarkup = createGalleryItem(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+new SimpleLightbox(".gallery a", { captionDelay: 250, captionsData: "alt" });
